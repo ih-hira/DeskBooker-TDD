32cbca6162d4c9f551e6491029e8c772c92cb6ed
ih-hira <imtiyaz.aust@gmail.com>
2021-03-09 00:13:19 +0600

    module 1 complete

---
+++ DeskBooker/DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,42 @@
+﻿using DeskBooker.Core.Domain;
+using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private readonly DeskBookingRequestProcessor _processor;
+
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Imtiyaz",
+                LastName = "Hossain",
+                Email = "ih_hira@outlook.com",
+                Date = new DateTime(2020, 1, 28)
+            };
+
+            //Act
+            DeskBookingResult result = _processor.BookDesk(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+            Assert.Equal("request", exception.ParamName);
+        }
+    }
+}
